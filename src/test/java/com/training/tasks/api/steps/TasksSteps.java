60168bdc1460b11f25e15b8c9c4f0be2ed4110b9
“Allan <allanfreitasqa@gmail.com>
2024-12-18 22:33:22 +0000

    :test_tube: test: validate POST endpoint with a valid payload

--- src/test/java/com/training/tasks/api/steps/TasksSteps.java
+++ src/test/java/com/training/tasks/api/steps/TasksSteps.java
@@ -1,7 +1,10 @@
 package com.training.tasks.api.steps;
 
 import io.cucumber.java.en.*;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.*;
 import static org.assertj.core.api.Assertions.*;
@@ -18,5 +21,11 @@ public class TasksSteps {
     @Then("the api returns with status code {int}")
     public void the_api_returns_status_code_of(int statusCode) {
         assertThat(response.getStatusCode()).isEqualTo(statusCode);
+    }
+
+    @When("POST request is made with a valid payload")
+    public void post_with_payload_file(){
+        File jsonData = new File("src/test/resources/payload.json");
+        response = given().contentType(ContentType.JSON).body(jsonData).when().post("/tasks");
     }
 }
--- src/test/resources/features/Tasks.feature
+++ src/test/resources/features/Tasks.feature
@@ -3,3 +3,7 @@ Feature: This is a sample features test
   Scenario: client makes call to GET /tasks
     When the client calls GET Tasks
     Then the api returns with status code 200
+
+  Scenario: Create a new Task
+    When POST request is made with a valid payload
+    Then the api returns with status code 200
